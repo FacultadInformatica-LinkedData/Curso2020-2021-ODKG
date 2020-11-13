c01308617d001382fbd791a59378f218e4858201
pablodonisebri <p.donis@alumnos.upm.es>
2020-11-13 12:23:07 +0100

    More queries for the app

--- HandsOn/Group08/app/queries_app.py
+++ HandsOn/Group08/app/queries_app.py
@@ -150,4 +150,131 @@ for r in g.query(q5):
   print(r.d, r.cases, r.p, r.nc)
 
 
+
+
+#Incidencia covid por comunidades
+q2 = prepareQuery('''
+  SELECT
+    ?iso ?fecha ?na ?np ?nh ?nu
+  WHERE {
+    
+    ?CCAA ex:hasCovidStatus  ?CS.
+    ?CCAA ex:hasISOCode ?iso.
+    ?CS ex:numPositiveAC ?na.
+    ?CS ex:numPositivePCR ?np.
+    ?CS ex:numberHospitalizations ?nh.
+    ?CS ex:numberUCI ?nu.
+    ?CS ex:inDate ?fecha .
+    
+  }
+  
+  ORDER BY ASC(?fecha)
+  LIMIT 30
+  ''',
+  initNs = { "s": s, "ex": ex}
+)
+
+print("\n Incidencia covid por comunidades ")
+for r in g.query(q2):
+  print(r.iso, r.fecha ,r.na, r.np, r.nh ,r.nu)
+
+
+
+#Incidencia covid por comunidades en una fecha dada
+q21 = prepareQuery('''
+  SELECT
+    ?iso ?na ?np ?nh ?nu
+  WHERE {
+    
+    
+    ?CCAA ex:hasCovidStatus  ?CS.
+    ?CCAA ex:hasISOCode ?iso.
+    ?CS ex:inDate '2020-04-06T00:00:00Z'^^s:Date.
+    ?CS ex:numPositiveAC ?na.
+    ?CS ex:numPositivePCR ?np.
+    ?CS ex:numberHospitalizations ?nh.
+    ?CS ex:numberUCI ?nu.
+    ?CS ex:inDate ?fecha .
+    
+  }
+  ''',
+  initNs = { "s": s, "ex": ex,"res":res}
+)
+
+print("\n Incidencia covid por comunidades en una fecha dada ")
+for r in g.query(q21):
+  print(r.iso ,r.na, r.np, r.nh ,r.nu)
+
+#Incidencia acumulada hasta el final de las fechas por comunidades
+q3 = prepareQuery('''
+  SELECT
+      ?iso ?link (SUM(?na) as ?nCount) (SUM(?np) as ?npCount) (SUM(?nh) as ?nhCount) (SUM(?nu) as ?nuCount)
+  WHERE {       
+    ?CCAA ex:hasISOCode ?iso.
+    ?CCAA o:sameAs ?link.
+    ?CCAA ex:hasCovidStatus ?CS.  
+    ?CS ex:numPositiveAC ?na.
+    ?CS ex:numPositiveAC ?na.  
+    ?CS ex:numPositivePCR ?np.
+    ?CS ex:numberHospitalizations ?nh.
+    ?CS ex:numberUCI ?nu. 
+    
+  }
+  GROUP BY ?CCAA
+  
+  ''',
+  initNs = { "s": s, "ex": ex,"o":o }
+)
+
+print("\n Incidencia covid  acumulada por comunidades hasta el final ")
+for r in g.query(q3):
+  print(r.iso ,r.link ,r.nCount,  r.npCount ,r.nhCount, r.nuCount)
+
+
+#numero de contratos por organizacion en el  total de las fechas
+q4 = prepareQuery('''
+  SELECT
+    ?name ?link (COUNT(?Order) as ?orderCount) (SUM(?coste) as ?ncoste)
+  WHERE {
+    ?company s:name ?name.
+    ?Order s:seller ?company.
+    ?Order ex:hasOrderAmount ?coste.
+    OPTIONAL { ?company o:sameAs  ?link. }
+  }
+  GROUP BY ?company
+  ORDER BY DESC(?orderCount)
+  LIMIT 10
+  ''',
+  initNs = { "s": s, "ex": ex, "o":o}
+)
+print("\n numero de contratos por organizacion en el  total de las fechas ")
+for r in g.query(q4):
+  print(r.name ,r.link ,r.orderCount, r.ncoste)
+
+
+#numero de contratos por organizacion en una fecha
+q41 = prepareQuery('''
+  SELECT
+    ?name ?link (COUNT(?Order) as ?orderCount) (SUM(?coste) as ?ncoste)
+  WHERE {
+    ?Order s:orderDate '2020-04-07T00:00:00Z'^^s:Date.
+    ?Order ex:hasOrderAmount ?coste.
+    ?Order s:seller ?company.
+    ?company s:name ?name.
+    OPTIONAL { ?company o:sameAs  ?link. }
+  }
+  GROUP BY ?company
+  ORDER BY DESC(?orderCount)
+  LIMIT 10
+  ''',
+  initNs = { "s": s, "ex": ex, "o":o}
+)
+
+print("\n #numero de contratos por organizacion en una fecha ")
+for r in g.query(q41):
+  print(r.name ,r.link ,r.orderCount, r.ncoste)
+
+
+
+
 exit(0)
