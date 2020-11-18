233813b9a5877fdeab209f83b3c1e0c1e475c666
Eduard Aymerich <eduardaymerich@gmail.com>
2020-11-18 21:53:05 +0100

    enh: Add sparql query get all contracts

--- HandsOn/Group01/app/js/main.js
+++ HandsOn/Group01/app/js/main.js
@@ -1,4 +1,136 @@
-var dataJSON = '[{"FeeType":"Domestic POS Declined Fee","FeeDescription":"per declined transaction","FeeAmount":"0.50"},{"FeeType":"Domestic PIN Declined Fee","FeeDescription":"per declined transaction","FeeAmount":"0.50"},{"FeeType":"International POS Declined Fee","FeeDescription":"per declined transaction","FeeAmount":"0.95"},{"FeeType":"International PIN Declined Fee ","FeeDescription":"per declined transaction","FeeAmount":"0.95"},{"FeeType":"ATM Domestic Fee <sup>1</sup>","FeeDescription":"One (1) no cost ATM withdrawal per deposit1, then $1.75 per transaction thereafter","FeeAmount":"1.75"},{"FeeType":"Domestic ATM Balance Inquiry Fee <sup>1</sup>","FeeDescription":"per ATM Balance Inquiry","FeeAmount":"0.50"},{"FeeType":"Domestic ATM Declined Fee <sup>1</sup>","FeeDescription":"per declined transaction","FeeAmount":"0.50"},{"FeeType":"International ATM Withdrawal Fee <sup>1</sup>","FeeDescription":"per transaction","FeeAmount":"3.00"},{"FeeType":"International ATM Balance Fee ","FeeDescription":"per ATM Balance Inquiry","FeeAmount":"0.95"},{"FeeType":"International ATM Declined Fee","FeeDescription":"per declined transaction","FeeAmount":"0.95"},{"FeeType":"OTC Withdrawal Fee","FeeDescription":"per transaction","FeeAmount":"4.00"},{"FeeType":"International OTC Withdrawal Fee","FeeDescription":"per transaction","FeeAmount":"4.00"},{"FeeType":"Currency Conversion Fee","FeeDescription":"3% of transaction amount","FeeAmount":"3.00%"},{"FeeType":"Card Replacement Fee","FeeDescription":"One (1) no cost replacement per calendar year or upon expiration; $5.00 per request thereafter for lost, stolen, and damaged cards.","FeeAmount":"5.00"},{"FeeType":" Expedited Card Replacement Fee ","FeeDescription":"$20.00 (per Card; an additional fee when a Card is reissued or replaced for any reason with requested expedited delivery)","FeeAmount":"20.00"},{"FeeType":"Check Refund Fee","FeeDescription":"$12.50 per refund check (When a refund check is issued for the remaining Card balance.","FeeAmount":"12.5"}]';
+var xmlhttp = new XMLHttpRequest();
+var url = "http://localhost:9000/sparql#query=PREFIX%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0APREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0ASELECT%20DISTINCT%20*%20WHERE%20%7B%0A%20%20%3Fcont%20a%2Frdfs%3AsubClassOf*%20%3Chttp%3A%2F%2Fcontsem.unizar.es%2Fdef%2Fsector-publico%2Fpproc%23Contract%3E%20.%0A%7D%0A";
+var dataJSON;
+
+function getData(cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      cb(xhr.responseText);
+    }
+  }
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+getData(function(text) {
+  console.log(text);
+})
+
+var dataObject = JSON.parse(dataJSON);
+var listItemString = $('#listItem').html();
+
+dataObject.forEach(buildNewList);
+$('#listItem').remove();
+
+/* Get unique list of elements */
+var lookupGovernmentRequester = {};
+var lookupAwardProcedure = {};
+var lookupTypeOfContract = {};
+var lookupCompany = {};
+
+var listGovernmentRequester = [];
+var listAwardProcedure = [];
+var listTypeOfContract = [];
+var listCompany = [];
+
+for (var item, i = 0; item = dataObject[i++];) {
+  console.log(item);
+  var gov      = item.GovernmentRequester;
+  var award    = item.AwardProcedure;
+  var contract = item.TypeOfContract;
+  var company  = item.Company;
+
+  if (!(gov in lookupGovernmentRequester)) {
+    lookupGovernmentRequester[gov] = 1;
+    listGovernmentRequester.push(gov);
+  }
+  if (!(award in lookupAwardProcedure)) {
+    lookupAwardProcedure[award] = 1;
+    listAwardProcedure.push(award);
+  }
+  if (!(contract in lookupTypeOfContract)) {
+    lookupTypeOfContract[contract] = 1;
+    listTypeOfContract.push(contract);
+  }
+  if (!(company in lookupCompany)) {
+    lookupCompany[company] = 1;
+    listCompany.push(company);
+  }
+}
+
+listGovernmentRequester.forEach(addFilterGov);
+function addFilterGov(item, index) {
+  var html = "<li>" +
+    '<input class="filter" data-filter=".' + item .replace(/\s/g, "")+ '" type="checkbox" id="' + item + '">' +
+    '<label class="checkbox-label" for="' + item.replace(/\s/g, "") + '">' + item + '</label>' +
+    "</li>";
+  $('#filter-GovernmentRequester').append(html);
+}
+
+listAwardProcedure.forEach(addFilterAward);
+function addFilterAward(item, index) {
+  var html = "<li>" +
+    '<input class="filter" data-filter=".' + item.replace(/\s/g, "") + '" type="checkbox" id="' + item + '">' +
+    '<label class="checkbox-label" for="' + item.replace(/\s/g, "") + '">' + item + '</label>' +
+    "</li>";
+  $('#filter-AwardProcedure').append(html);
+}
+
+listTypeOfContract.forEach(addFilterContract);
+function addFilterContract(item, index) {
+  var html = '<li class="filter" data-filter=".' + item.replace(/\s/g, "") + '"><a href="#0" data-type="' + item + '">' +
+    item + '</a></li>';
+  $('#filter-TypeOfContract').append(html);
+}
+
+listCompany.forEach(addFilterCompany);
+function addFilterCompany(item, index) {
+  var html = "<li>" +
+    '<input class="filter" data-filter=".' + item.replace(/\s/g, "") + '" type="checkbox" id="' + item + '">' +
+    '<label class="checkbox-label" for="' + item.replace(/\s/g, "") + '">' + item + '</label>' +
+    "</li>";
+  $('#filter-Company').append(html);
+}
+
+function buildNewList(item, index) {
+  var cssItem = "mix " +
+    item.Code.replace(/\s/g, "") + " " +
+    item.GovernmentRequester.replace(/\s/g, "") + " " +
+    item.AwardProcedure.replace(/\s/g, "") + " " +
+    item.TypeOfContract.replace(/\s/g, "") + " " +
+    item.BiddingPrice.replace(/\s/g, "") + " " +
+    item.AwardPrice.replace(/\s/g, "") + " " +
+    item.Company.replace(/\s/g, "");
+  var listItem = $('<li class="' + cssItem + '">' + listItemString + '</li>');
+
+  var listItemCode = $('.code', listItem);
+  listItemCode.html(item.Code);
+  var listItemDescription = $('.description', listItem);
+  listItemDescription.html(item.Description);
+  var listItemGovernmentRequester = $('.governmentRequester', listItem);
+  var govItem = item.GovernmentRequester;
+  if( item.wikiGovernmentRequester ) {
+    govItem = "<a href=" + item.wikiGovernmentRequester + ' target="_blank">' + item.GovernmentRequester + "</a>";
+    console.log(govItem);
+  }
+  listItemGovernmentRequester.html(govItem);
+  var listItemAwardProcedure = $('.awardProcedure', listItem);
+  listItemAwardProcedure.html(item.AwardProcedure);
+  var listItemTypeOfContract = $('.typeOfContract', listItem);
+  listItemTypeOfContract.html(item.TypeOfContract);
+  var listItemBiddingPrice = $('.biddingPrice', listItem);
+  listItemBiddingPrice.html(item.BiddingPrice + ' €');
+  var listItemAwardPrice = $('.awardPrice', listItem);
+  listItemAwardPrice.html(item.AwardPrice + ' €');
+  var listItemCompany = $('.company', listItem);
+  var companyItem = item.Company;
+  if( item.wikiCompany ) {
+    companyItem = "<a href=" + item.wikiCompany + ' target="_blank">' + item.Company + "</a>";
+    console.log(companyItem);
+  }
+  listItemCompany.html(companyItem);
+  $('#dataList').append(listItem);
+}
 
 jQuery(document).ready(function($){
 	//open/close lateral filter
